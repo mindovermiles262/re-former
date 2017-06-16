5a05945a441511e3351643602df27bbc8434c5fc
Andy Duss <mindovermiles262@gmail.com>
2017-06-16 13:27:36 -0700

    finished #new forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
   def new
-    #
+    @user = User.new()
   end
 
   def create
-    #
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_parameters)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+
+  def user_parameters
+    params.require(:user).permit(:username,:email,:password)
   end
 
 end
