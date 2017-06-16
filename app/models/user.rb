3c40671921233d6616461699cb94ce4d59901c5a
Andy Duss <mindovermiles262@gmail.com>
2017-06-16 14:47:53 -0700

    finished #edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true
   validates :email, presence:true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   has_secure_password
 end
